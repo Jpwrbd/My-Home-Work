3802da7133c8dc3c06137561506a86f988019e90
Papawarin Bussadee <119738757+Jpwrbd@users.noreply.github.com>
2023-12-18 13:47:48 +0700

    Update Lab 1.1.c

--- Lab 1.1.c
+++ Lab 1.1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main() {
     int n ;
-    printf("Enter a number: ");
-    scanf("%d", &n);
+    printf("Enter a number: ") ;
+    scanf("%d", &n) ;
     for (int i = 1; i <=n; i++) {
         printf("[%d] Hello World\n", i) ;
     }
-    return 0;
+    return 0 ;
 }
